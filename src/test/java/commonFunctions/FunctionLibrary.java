094787584c1353b757213fc9ff1f6ee3880de94f
Subramani Shanmugam <subramani.shanmugam@gmail.com>
2023-04-27 19:51:04 +0100

    DataEngine done

--- src/test/java/commonFunctions/FunctionLibrary.java
+++ src/test/java/commonFunctions/FunctionLibrary.java
@@ -1,5 +1,25 @@
 package commonFunctions;
 
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+
 public class FunctionLibrary {
+	public static WebDriver driver;
+	public static Properties conpro;
+	public static String Expected =" ";
+	public static String Actual = " ";
+	
+	//method for launch browser
+	
+	public static WebDriver startBrowser() throws Throwable{
+		conpro =new Properties();
+		return driver;
+		
+		
+		
+	}
+	
+	
 
 }
